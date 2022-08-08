75ffa7e3811c6438d03f75efbf54f269c1adb304
Diana Mendoza <diana.pat.mendoza@gmail.com>
2022-08-07 20:11:06 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string> {"Call of Duty", "Mario Bros", "Just Dance", "Fall Guys" };
+
+
+            var sorted = videoGames.OrderBy(x => x.Length).ToList<string>();
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
